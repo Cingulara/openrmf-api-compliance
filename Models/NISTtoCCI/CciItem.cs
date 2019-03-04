7b1f099e8caa9cdc7b53a52a8d2d4bc0a31ca894
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-03-04 07:10:10 -0500

    Finalizing the ingest of the CCI to NIST XML
    Need to run through the compliance checker based on the CCI
    matching to this list. Then create the Compliance Listing
    based on the actual Major Controls as the main node. Each
    child record of it has 1 or more STIG checklist items
    matching the CCI to that Major control.

--- Models/NISTtoCCI/CciItem.cs
+++ Models/NISTtoCCI/CciItem.cs
@@ -10,15 +10,21 @@ namespace openstig_api_compliance.Models.NISTtoCCI
             references = new List<CciReference>();
         }
         public string cciId { get; set; }
+        public string status { get; set; }
         public string publishDate { get; set; }
         public string contributor { get; set; }
-        public string Definition { get; set; }
+        public string definition { get; set; }
         public string type { get; set; }
+        public string parameter { get; set; }
+        public string note { get; set; }
         public List<CciReference> references { get; set; }
     }
 
     public class CciReference
     {
+        public CciReference()
+        {}
+        
         public string creator { get; set; }
         public string title { get; set; }
         public string version { get; set; }
