3801c534debc28645b590a9f7b70fd3206f0123a
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-26 18:01:41 -0500

    adding the copyright notice

--- src/Models/Compliance/ComplianceRecord.cs
+++ src/Models/Compliance/ComplianceRecord.cs
@@ -1,3 +1,9 @@
+// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
+
+// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
+
 using System;
 
 namespace openrmf_api_compliance.Models.Compliance
